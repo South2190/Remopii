0f3eb8bb12216c48558469861d5af4595017e1ad
LAPTOP-U88FK0DK\大津留　泰治 <DOMI-and103-@outlook.jp>
2024-11-14 11:50:57 +0900

    スマホ画面

---
+++ rimocon/sutdy.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta charset="utf-8">
+    <title>test</title>
+   
+    <style>
+      .btn{
+        text-align: center;
+      }
+
+      .btn1 {
+        margin-right: 3%; 
+        display: inline-block;
+        width: 40%;
+        padding: 13% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #63bbde;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+        
+      }
+      .btn1:active{
+      background: #325f70;
+      color: #FFF;
+      }
+
+      .btn2 {
+        display: inline-block;
+        width: 40%;
+        padding: 13% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #6cc786;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+        
+      }
+      .btn2:active{
+      background: #335e3f;
+      color: #FFF;
+      }
+
+      .btn3{
+        margin-top: 10%;
+        margin-right: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn3:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+      .btn4{
+        margin-top: 10%;
+        margin-right: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn4:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+      .btn5{
+        margin-top: 10%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn5:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+
+      .btn6{
+        margin-top: 4.5%;
+        margin-right: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn6:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+      .btn7{
+        margin-top: 4.5%;
+        margin-right: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn7:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+      .btn8{
+        margin-top: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn8:active{
+      background: #7b867e;
+      color: #FFF;
+
+      }.btn9{
+        margin-top: 4.5%;
+        margin-right: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn9:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+      .btn10{
+        margin-top: 4.5%;
+        margin-right: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn10:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+      .btn11{
+        margin-top: 4.5%;
+        display: inline-block;
+        width: 24%;
+        padding: 5% 0;
+        text-decoration: none;
+        text-align: center;
+        color: #000;
+        background-color: #bfcec3;
+        border-radius: 7px;
+        box-shadow: 2px 2px gray;
+      }
+      .btn11:active{
+      background: #7b867e;
+      color: #FFF;
+      }
+    </style>
+
+  </head>
+
+
+  <div class="btn">
+  <a href="#" class="btn1">戻る</a>
+  <a href="#" class="btn2">進む</a>
+  
+  <a href="#" class="btn3">あ</a>
+  <a href="#" class="btn4">い</a>
+  <a href="#" class="btn5">う</a>
+
+  <a href="#" class="btn6">え</a>
+  <a href="#" class="btn7">お</a>
+  <a href="#" class="btn8">か</a>
+
+  <a href="#" class="btn9">き</a>
+  <a href="#" class="btn10">く</a>
+  <a href="#" class="btn11">け</a>
+
+  </div>
+  
+</html>
