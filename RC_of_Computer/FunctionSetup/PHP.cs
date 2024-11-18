0bdde7a43f420a82789ce52fd943113531430a4e
SHELL <south02274653@gmail.com>
2024-11-18 09:31:37 +0900

    C#の言語バージョンを9.0に変更、他
    - 余計な画面の拡大率設定を削除
    - PHPの自動セットアップ機能の制作途中

--- RC_of_Computer/FunctionSetup/PHP.cs
+++ RC_of_Computer/FunctionSetup/PHP.cs
@@ -1,6 +1,8 @@
 ﻿using System.Drawing;
 using System.Windows.Forms;
 
+using RC_of_Computer.Classes;
+
 namespace RC_of_Computer.FunctionSetup
 {
     public partial class PHP : Form
@@ -9,10 +11,19 @@ namespace RC_of_Computer.FunctionSetup
         {
             InitializeComponent();
 
-            Bitmap DirIcon = Classes.GetIcon.GetBitmapFromEXEDLL(Program.SHELL32DLL, 3, false);
+            Bitmap DirIcon = GetIcon.GetBitmapFromEXEDLL(Program.SHELL32DLL, 3, false);
             PHPRef.Image = DirIcon;
             DocumentRootRef.Image = DirIcon;
-            RunPHPSetup.Image = Classes.GetIcon.GetBitmapFromEXEDLL(Program.IMAGERESDLL, 73, false);
+            RunPHPSetup.Image = GetIcon.GetBitmapFromEXEDLL(Program.IMAGERESDLL, 73, false);
+        }
+
+        private void RunPHPSetup_Click(object sender, System.EventArgs e)
+        {
+            PHPAutoSetup setupWindow = new()
+            {
+                Owner = this
+            };
+            _ = setupWindow.ShowDialog();
         }
     }
 }
