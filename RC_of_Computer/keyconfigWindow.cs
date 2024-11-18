99acda6434cf3d964fa4d5594147ca9de7ebf52b
SHELL <south02274653@gmail.com>
2024-11-18 11:32:47 +0900

    チェックボックスの処理実装の下準備

--- RC_of_Computer/keyconfigWindow.cs
+++ RC_of_Computer/keyconfigWindow.cs
@@ -11,10 +11,22 @@ namespace RC_of_Computer
             InitializeComponent();
         }
 
+        /// <summary>
+        /// すべての"変更"ボタンから呼び出されるイベント
+        /// </summary>
         private void keyChange_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             Debug.WriteLine(button.Name);
+        }
+
+        /// <summary>
+        /// すべての"Hidden"チェックボックスのチェック状態が変わった際に呼び出されるイベント
+        /// </summary>
+        private void Hidden_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender;
+            Debug.WriteLine(checkBox.Name + ": " + checkBox.Checked);
         }
     }
 }
