9045c667a9e9945fca3733ea90fbf172a3da5e3a
LAPTOP-U88FK0DK\大津留　泰治 <DOMI-and103-@outlook.jp>
2024-11-25 11:42:09 +0900

    微調整

--- rimocon/sumahogamen.php
+++ rimocon/sumahogamen.php
@@ -70,7 +70,7 @@
   <head>
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta charset="utf-8">
-    <title>test</title>
+    <title>リモコン</title>
 
     <style>
       body{
@@ -84,6 +84,7 @@
 
       .mainbtn1 {
         margin-right: 3%;
+        margin-top: 3.5%;
         margin-bottom: 5.5%; 
         display: inline-block;
         width: 46%;
@@ -102,6 +103,7 @@
       }
 
       .mainbtn2 {
+        margin-top: 3.5%;
         margin-bottom: 5.5%;
         display: inline-block;
         width: 46%;
