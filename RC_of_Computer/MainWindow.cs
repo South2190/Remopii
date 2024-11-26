77825586bcc54585b6e1e03bccdf7628471f0503
Syatyo0401 <ttsy.nktn@gmail.com>
2024-11-26 11:51:17 +0900

    接続するIPアドレス表示機能追加

--- RC_of_Computer/MainWindow.cs
+++ RC_of_Computer/MainWindow.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using System.Net;
+using System;
 
 namespace RC_of_Computer
 {
@@ -40,7 +42,10 @@ namespace RC_of_Computer
 
         private void ServerIO_Click(object sender, System.EventArgs e)
         {
-
+            string hostname = Dns.GetHostName();
+            IPAddress[] adrList = Dns.GetHostAddresses(hostname);
+            string url = (Properties.Settings.Default.PortNumber == 80) ? $"http://{adrList[1].ToString()}" : $"http://{adrList[1].ToString()}:{Properties.Settings.Default.PortNumber.ToString()}";
+            MessageBox.Show(url);
         }
 
         /// <summary>
