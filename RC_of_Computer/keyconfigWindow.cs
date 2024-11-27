daaaadd1ae36a44691ab62ed6d5661e4133bc5b7
DESKTOP-4LPPAV7\kaito <kaito2930@icloud.com>
2024-11-27 11:50:05 +0900

    キー入力を数字にも対応させた

--- RC_of_Computer/keyconfigWindow.cs
+++ RC_of_Computer/keyconfigWindow.cs
@@ -179,8 +179,38 @@ namespace RC_of_Computer
                     return "{F15}";
                 case Keys.F16:
                     return "{F16}";
+                case Keys.D0:
+                case Keys.NumPad0:
+                    return "0";
+                case Keys.D1:
+                case Keys.NumPad1:
+                    return "1";
+                case Keys.D2:
+                case Keys.NumPad2:
+                    return "2";
+                case Keys.D3:
+                case Keys.NumPad3:
+                    return "3";
+                case Keys.D4:
+                case Keys.NumPad4:
+                    return "4";
+                case Keys.D5:
+                case Keys.NumPad5:
+                    return "5";
+                case Keys.D6:
+                case Keys.NumPad6:
+                    return "6";
+                case Keys.D7:
+                case Keys.NumPad7:
+                    return "7";
+                case Keys.D8:
+                case Keys.NumPad8:
+                    return "8";
+                case Keys.D9:
+                case Keys.NumPad9:
+                    return "9";
                 case Keys.Up:
-                    return "UP";
+                    return "{UP}";
                 case Keys.Down:
                     return "{DOWN}";
                 case Keys.Left:
@@ -201,6 +231,8 @@ namespace RC_of_Computer
                     return "{ENTER}";
                 case Keys.Escape:
                     return "{ESC}";
+                case Keys.Help:
+                    return "{HELP}";
                 case Keys.Home:
                     return "{HOME}";
                 case Keys.Insert:
@@ -214,6 +246,8 @@ namespace RC_of_Computer
                 case Keys.PrintScreen:
                     // 検出不可
                     return "{PRTSC}";
+                case Keys.Scroll:
+                    return "{SCROLLLOCK}";
                 case Keys.Tab:
                     return "{TAB}";
                 default:
