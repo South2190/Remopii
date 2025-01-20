89686fb016bcb97f9107e841a752848144996749
SHELL <south02274653@gmail.com>
2025-01-20 09:16:06 +0900

    大きな文字で表示する機能を完全に削除

--- Remopii/FunctionSetup/ProcessSearch.Designer.cs
+++ Remopii/FunctionSetup/ProcessSearch.Designer.cs
@@ -59,8 +59,8 @@
             // ProcessSearch
             // 
             this.AcceptButton = this.okButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(419, 76);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.appListComboBox);
